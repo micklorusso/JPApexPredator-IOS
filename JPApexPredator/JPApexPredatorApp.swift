a7fe8bd9505f82c7a128376c536baa831f406a7d
milorusso <milorusso@deloitte.it>
2024-12-10 15:18:38 +0100

    Initial Commit

---
+++ JPApexPredator/JPApexPredatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorApp.swift
+//  JPApexPredator
+//
+//  Created by Lorusso, Michele on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
